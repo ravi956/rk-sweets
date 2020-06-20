7fbb964883aed01be0f3ca7ffe208aa5bea9340f
Ravi Kumar <ravikumar956078@gmail.com>
2020-06-20 17:06:39 +0530

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	if( $(window).width() < 768 ) {
+            $('#raghupati').removeClass('show');
+        }
     $(window).resize(function() {
         if( $(this).width() < 768 ) {
             $('#raghupati').removeClass('show');
